573b67d5c3934cfa59c5b54c3893a96053b65507
dpetr <xpetrickod@stuba.sk>
2022-03-01 18:00:51 +0100

    prvy commit

---
+++ src/MainPackage/SipMain.java
@@ -0,0 +1,17 @@
+package MainPackage;
+
+import org.mjsip.sip.provider.SipProvider;
+import org.mjsip.server.ServerProfile;
+import org.mjsip.server.StatefulProxy;
+
+
+public class SipMain {
+
+    public static void main(String [] args) {
+        SipProvider sipProvider = new SipProvider("192.168.100.18", 5060);
+        ServerProfile serverProfile = new ServerProfile(null);
+        StatefulProxy stateFullProxy = new StatefulProxy(sipProvider, serverProfile);
+
+    }
+}
+
